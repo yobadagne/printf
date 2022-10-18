a9fa097a430341dc29ae1ae4fb369802aec56d62
Firaol-Dagne <firaoldagne85@gmail.com>
2022-10-18 19:34:52 +0000

    main

--- handle.c
+++ handle.c
@@ -1,4 +1,10 @@
 #include "main.h"
+unsigned char handle_flags(const char *flag, char *index);
+unsigned char handle_length(const char *modifier, char *index);
+int handle_width(va_list args, const char *modifier, char *index);
+int handle_precision(va_list args, const char *modifier, char *index);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char);
 /**
  * handle_flags - Matches flags with corresponding values.
  * @flag: A pointer to a potential string of flags.
@@ -164,18 +170,7 @@ unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 	converter_t converters[] = {
 		{'c', convert_c},
 		{'s', convert_s},
-		{'d', convert_di},
-		{'i', convert_di},
 		{'%', convert_percent},
-		{'b', convert_b},
-		{'u', convert_u},
-		{'o', convert_o},
-		{'x', convert_x},
-		{'X', convert_X},
-		{'S', convert_S},
-		{'p', convert_p},
-		{'r', convert_r},
-		{'R', convert_R},
 		{0, NULL}
 	};
 
