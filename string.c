69c8e54160059dfb4b3f8bc438253dc02e45b2bb
yobadagne <yobadagne@gmail.com>
2022-10-18 19:00:37 +0000

    string

---
+++ string.c
@@ -0,0 +1,44 @@
+#include "mian.h"
+/**
+ * convert_s - Converts an argument to a string and
+ *             stores it to a buffer contained in a struct.
+ * @args: A va_list pointing to the argument to be converted.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @len: A length modifier.
+ * @output: A buffer_t struct containing a character array.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
+unsigned int convert_s(va_list args, buffer_t *output,
+		unsigned char flags, int wid, int prec, unsigned char len)
+{
+	char *str, *null = "(null)";
+	int size;
+	unsigned int ret = 0;
+
+	(void)flags;
+	(void)len;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		return (_memcpy(output, null, 6));
+
+	for (size = 0; *(str + size);)
+		size++;
+
+	ret += print_string_width(output, flags, wid, prec, size);
+
+	prec = (prec == -1) ? size : prec;
+	while (*str != '\0' && prec > 0)
+	{
+		ret += _memcpy(output, str, 1);
+		prec--;
+		str++;
+	}
+
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
+}
